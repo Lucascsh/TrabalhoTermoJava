79099e50c933f8bcf91d53ae8ca09f48e0bba016
Lucascsh <lucasceraschaefer@gmail.com>
2022-07-04 17:14:27 -0300

    Atualização

--- ProjetoTermo/src/Teste.java
+++ ProjetoTermo/src/Teste.java
@@ -14,7 +14,7 @@ public class Teste {
             Scanner leitor = new Scanner(obj);
             while (leitor.hasNextLine()) {
                 String data = leitor.nextLine();
-                dis.add(data);
+                dis.add(data.toUpperCase());
             }
 
             leitor.close();
@@ -24,34 +24,39 @@ public class Teste {
             e.printStackTrace();
         } 
         String palavraCerta = dis.get((int)(Math.random()*6026));
+        palavraCerta.toUpperCase();
         System.out.println(palavraCerta);
-        
-        System.out.println("_ _ _ _ _");
         int tentativas = 0;
         
         do{
-            String palavraEscolhida = JOptionPane.showInputDialog("Digite uma palavra com 5 letras: ");
-            
+            String palavraEscolhida = JOptionPane.showInputDialog("Digite uma palavra com 5 letras: ").toUpperCase();
             if(palavraEscolhida.length() !=5){
                 JOptionPane.showMessageDialog(null, "Palavra Inválida, digite outra!");
-                
             }else{
-                tentativas ++;
                 System.out.println(palavraEscolhida);
-                //%d %d %d %d %d if(char = palavra) 
-                //a  _  _   _   _
-            } 
-
-            if (palavraEscolhida.equals(palavraCerta)){
-                JOptionPane.showMessageDialog(null, "Parabéns você acertou a palavra!");
-                break;
-            }
-            
-            if (tentativas == 2){
-                JOptionPane.showMessageDialog(null, "Acabaram as tentativas! ");
-            }
-        
-        }while(tentativas < 2);
+                if (!dis.contains(palavraEscolhida)){
+                    JOptionPane.showMessageDialog(null, "Palavra não Reconhecida.\nTente outra!");
+                    continue;
+                }else if(palavraEscolhida.equals(palavraCerta)){
+                    JOptionPane.showMessageDialog(null, "Parabéns você acertou a palavra!");
+                        break;
+                }else
+                    tentativas++;
+                    int percorrerLetras = 0;
+                    do{
+                        if(palavraEscolhida.charAt(percorrerLetras)==palavraCerta.charAt(percorrerLetras)){
+                            System.out.println("A letra " + palavraEscolhida.charAt(percorrerLetras) + " está na palavra e na posição correta");
+                        }else if (palavraCerta.contains("" + palavraEscolhida.charAt(percorrerLetras))){
+                            System.out.println("A letra " + palavraEscolhida.charAt(percorrerLetras) + " está na palavra, mas na posição incorreta");
+                        }else if (!palavraCerta.contains("" + palavraEscolhida.charAt(percorrerLetras))){
+                            System.out.println("A letra " + palavraEscolhida.charAt(percorrerLetras) + " não está na palavra");
+                        }
+                        percorrerLetras++;
+                        
+                        }while(percorrerLetras<5);    
+               }
+     
+        }while(tentativas < 5);
         
         
     }
